6670264198fd17e84135711b791885ac0907b6c9
masopego <masopego@gmail.com>
2020-07-09 19:39:33 +0200

    Conect fetch with API series

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const searchForm = document.querySelector('.js-search__form');
+const searchButton = document.querySelector('.js-search__button');
+
+function hunterSeries(ev) {
+  ev.preventDefault();
+  let userValue = searchForm.value;
+  getSeries(userValue);
+}
+
+function getSeries(name) {
+  fetch(`http://api.tvmaze.com/search/shows?q=${name}`)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    });
+}
+
+searchButton.addEventListener('click', hunterSeries);
