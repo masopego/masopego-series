eba751689dafffe49b7c511936aeca7d22ec2e2d
masopego <masopego@gmail.com>
2020-07-09 20:18:53 +0200

    Draw series into HTML

--- src/js/main.js
+++ src/js/main.js
@@ -2,11 +2,17 @@
 
 const searchForm = document.querySelector('.js-search__form');
 const searchButton = document.querySelector('.js-search__button');
+const searchResults = document.querySelector('.js-search__container');
 
 function hunterSeries(ev) {
   ev.preventDefault();
   let userValue = searchForm.value;
   getSeries(userValue);
+  clearResults();
+}
+
+function clearResults() {
+  searchResults.innerHTML = '';
 }
 
 function getSeries(name) {
@@ -14,7 +20,21 @@ function getSeries(name) {
     .then((response) => response.json())
     .then((data) => {
       console.log(data);
+      const seriesList = document.createElement('ul');
+      let listElement;
+      data.forEach((element) => {
+        listElement += renderSingleElement(element);
+      });
+      seriesList.innerHTML = listElement;
+      searchResults.appendChild(seriesList);
     });
 }
 
 searchButton.addEventListener('click', hunterSeries);
+
+function renderSingleElement(element) {
+  return `<li>
+    <img src="${element.show.image.medium}"/>
+    <p>${element.show.name}</p>
+    </li>`;
+}
