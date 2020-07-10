3d948aa95896451b132d884270f156f0f7f7dd80
masopego <masopego@gmail.com>
2020-07-10 16:29:33 +0200

    Refactoring methods names

--- src/js/main.js
+++ src/js/main.js
@@ -4,50 +4,47 @@ const searchForm = document.querySelector('.js-search__form');
 const searchButton = document.querySelector('.js-search__button');
 const searchResults = document.querySelector('.js-search__container');
 
+searchButton.addEventListener('click', onSearch);
+
 function onSearch(ev) {
   ev.preventDefault();
-  let userValue = searchForm.value;
-  getSeries(userValue);
   clearResults();
+  getSeries(searchForm.value);
 }
 
 function clearResults() {
   searchResults.innerHTML = '';
 }
 
-function getSeries(name) {
-  fetch(`http://api.tvmaze.com/search/shows?q=${name}`)
+function getSeries(search) {
+  fetch(`http://api.tvmaze.com/search/shows?q=${search}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
-
+      console.log('Results', data);
       if (data.length === 0) {
-        gettextNotResult();
+        printNotResults();
       } else {
-        const seriesList = document.createElement('ul');
-        seriesList.classList.add('main--list');
-        let listElement = '';
-        console.log(listElement);
-
-        data.forEach((element) => {
-          listElement += renderSingleElement(element.show);
-        });
-        seriesList.innerHTML = listElement;
-        searchResults.appendChild(seriesList);
-
-        let seriesElement = document.querySelectorAll('.js-list--element');
-        seriesElement.forEach((element) => {
-          element.addEventListener('click', onClickSeries);
-        });
-      }
-
-      function onClickSeries(ev) {
-        ev.currentTarget.classList.toggle('js-selected');
+        printSeries(data);
       }
     });
 }
 
-searchButton.addEventListener('click', onSearch);
+function printSeries(data) {
+  const seriesList = document.createElement('ul');
+  seriesList.classList.add('main--list');
+  let listElement = '';
+
+  data.forEach((element) => {
+    listElement += renderSingleElement(element.show);
+  });
+  seriesList.innerHTML = listElement;
+  searchResults.appendChild(seriesList);
+
+  let seriesElement = document.querySelectorAll('.js-list--element');
+  seriesElement.forEach((element) => {
+    element.addEventListener('click', onClickSeries);
+  });
+}
 
 function renderSingleElement(element) {
   const image = element.image ? element.image.medium : 'placeholder';
@@ -60,8 +57,12 @@ function renderSingleElement(element) {
     </li>`;
 }
 
-function gettextNotResult() {
+function printNotResults() {
   let textNotResult = document.createElement('p');
   textNotResult.innerHTML = 'No hay resultados. Por favor, revisa la búsqueda.';
   searchResults.appendChild(textNotResult);
+}
+
+function onClickSeries(ev) {
+  ev.currentTarget.classList.toggle('js-selected');
 }
